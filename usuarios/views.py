edcd73928df725333e0fa8e683f332e6349db1f4
Carlo Tamayo <cejota708@gmail.com>
2025-08-09 13:04:59 -0600

    modal, historial, dashboard, admin-user y creo que ya

--- usuarios/views.py
+++ usuarios/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.contrib.auth import login
 from django.urls import reverse
 from articulos.models import Articulo
+from comentarios.models import Comentario
 from usuarios.forms import RegistroUsuarioForm
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
@@ -58,14 +59,24 @@ def redireccion_dashboard(request):
 
 @login_required
 def dashboard_admin(request):
+    articulos_usuario = Articulo.objects.filter(autor=request.user)
+    comentarios_usuario = Comentario.objects.filter(usuario=request.user).order_by('-fecha_creacion')[:5]
     if not ( request.user.is_superuser or request.user.is_staff):
         return HttpResponseForbidden("No tienes permiso para ver este dashboard.")
-    return render(request, 'usuarios/dashboard_admin.html')
+    return render(request, 'usuarios/dashboard_admin.html',{
+        'articulos': articulos_usuario,
+        'comentarios': comentarios_usuario,
+    })
 
 @login_required
 def dashboard_cliente(request):
+    articulos_usuario = Articulo.objects.filter(autor=request.user)
+    comentarios_usuario = Comentario.objects.filter(usuario=request.user).order_by('-fecha_creacion')[:5]
     if request.user.is_superuser or request.user.is_staff:
         return HttpResponseForbidden("No tienes permiso para ver este dashboard.")
-    return render(request, 'usuarios/dashboard_cliente.html') 
+    return render(request, 'usuarios/dashboard_cliente.html',{
+        'articulos': articulos_usuario,
+        'comentarios': comentarios_usuario,
+    }) 
 
 
